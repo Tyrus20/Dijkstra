ab70f74bd28cbaf3c2edca7edeeccfbc54365f69
Tyrus Kelly <tyrus@email.unc.edu>
2022-05-12 02:28:03 -0400

    wrote the README.md explaining the project

--- main/Main.java
+++ main/Main.java
@@ -3,67 +3,71 @@ package main;
 public class Main {
 
   public static void main(String[] args) {
+    //airport map of the US
     Graph graph1 = new GraphImpl();
 
-    //nodes
-    graph1.addNode("A");
-    graph1.addNode("B");
-    graph1.addNode("C");
-    graph1.addNode("D");
-    graph1.addNode("E");
-    graph1.addNode("F");
-
-    //edges
-    graph1.addEdge("A", "B", 2.0);
-    graph1.addEdge("A", "D", 1.0);
-    graph1.addEdge("B", "E", 5.0);
-    graph1.addEdge("C", "E", 4.0);
-    graph1.addEdge("C", "F", 6.0);
-    graph1.addEdge("D", "B", 3.0);
-    graph1.addEdge("D", "C", 3.0);
-    graph1.addEdge("E", "F", 5.0);
-
-    //source node
-    String source1 = "A";
-
-    System.out.println();
-    System.out.println("Graph 1");
-    System.out.println("Length of shortest path from source node to destination node: ");
-    System.out.println(graph1.dijkstra(source1));
-
-    /*------------------------------------------------------------------------------------------*/
-    Graph graph2 = new GraphImpl();
-
     //airports
-    graph2.addNode("DEN"); //Denver International Airport - Denver, Colorado
-    graph2.addNode("IAH"); //George Bush Intercontinental Airport - Houston, Texas
-    graph2.addNode("JFK"); //John F. Kennedy International Airport - New York City, New York
-    graph2.addNode("LAX"); //Los Angeles International Airport - Los Angeles, California
-    graph2.addNode("MIA"); //Miami International Airport - Miami, Florida
-    graph2.addNode("ORD"); //O'Hare International Airport - Chicago, Illinois
-    graph2.addNode("SEA"); //Seattle-Tacoma International Airport - Seattle, Washington
+    graph1.addNode("DEN"); //Denver International Airport - Denver, Colorado
+    graph1.addNode("IAH"); //George Bush Intercontinental Airport - Houston, Texas
+    graph1.addNode("JFK"); //John F. Kennedy International Airport - New York City, New York
+    graph1.addNode("LAX"); //Los Angeles International Airport - Los Angeles, California
+    graph1.addNode("MIA"); //Miami International Airport - Miami, Florida
+    graph1.addNode("ORD"); //O'Hare International Airport - Chicago, Illinois
+    graph1.addNode("SEA"); //Seattle-Tacoma International Airport - Seattle, Washington
 
     //flights
-    graph2.addEdge("JFK", "MIA", 1089);
-    graph2.addEdge("JFK", "ORD", 740);
-    graph2.addEdge("ORD", "MIA", 1197);
-    graph2.addEdge("ORD", "DEN", 888);
-    graph2.addEdge("DEN", "SEA", 1024);
-    graph2.addEdge("DEN", "LAX", 862);
-    graph2.addEdge("SEA", "LAX", 954);
-    graph2.addEdge("MIA", "IAH", 964);
-    graph2.addEdge("DEN", "IAH", 862);
-    graph2.addEdge("ORD", "IAH", 862);
+    graph1.addEdge("JFK", "MIA", 1089);
+    graph1.addEdge("JFK", "ORD", 740);
+    graph1.addEdge("ORD", "MIA", 1197);
+    graph1.addEdge("ORD", "DEN", 888);
+    graph1.addEdge("DEN", "SEA", 1024);
+    graph1.addEdge("DEN", "LAX", 862);
+    graph1.addEdge("SEA", "LAX", 954);
+    graph1.addEdge("MIA", "IAH", 964);
+    graph1.addEdge("DEN", "IAH", 862);
+    graph1.addEdge("ORD", "IAH", 862);
 
     //starting airport
     String source2 = "MIA";
 
     System.out.println();
-    System.out.println("Graph 2");
+    System.out.println("Graph 1");
     System.out.println("Length of shortest path from source (" + source2 + ") to destination (in statute miles): ");
-    System.out.println(graph2.dijkstra(source2));
+    System.out.println(graph1.dijkstra(source2));
 
     /*------------------------------------------------------------------------------------------*/
+    //example node graph
+    Graph graph2 = new GraphImpl();
+
+    //nodes
+    graph2.addNode("A");
+    graph2.addNode("B");
+    graph2.addNode("C");
+    graph2.addNode("D");
+    graph2.addNode("E");
+    graph2.addNode("F");
+
+    //edges
+    graph2.addEdge("A", "B", 2.0);
+    graph2.addEdge("A", "C", 4.0);
+    graph2.addEdge("B", "C", 1.0);
+    graph2.addEdge("B", "D", 4.0);
+    graph2.addEdge("B", "E", 2.0);
+    graph2.addEdge("C", "E", 3.0);
+    graph2.addEdge("D", "E", 3.0);
+    graph2.addEdge("D", "F", 2.0);
+    graph2.addEdge("E", "F", 2.0);
+
+    //source node
+    String source1 = "A";
+
+    System.out.println();
+    System.out.println("Graph 2");
+    System.out.println("Length of shortest path from source node to destination node: ");
+    System.out.println(graph2.dijkstra(source1));
+
+    /*------------------------------------------------------------------------------------------*/
+    //feel free to test out your own graph
     Graph graph3 = new GraphImpl();
 
     //nodes
--- main/README.md
+++ main/README.md
@@ -1,4 +1,10 @@
 # Dijkstra
 Dijkstra's shortest path algorithm calculates the shortest path between two nodes in an undirected graph.
 
+To see this algorithm in action take a look at the Main file.
 
+Graph 1 represents a map of airports in the US where the nodes represent airports and the edges represent flights between them.
+
+Graph 2 is a simple undirected graph.
+
+Graph 3 is a blank template to allow you to test your own graph.
